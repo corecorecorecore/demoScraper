fa5765963afbdf836832f469de2409c91ba10353
/⁶⁶⁶Bunti <58895443+bntr00t@users.noreply.github.com>
2022-03-05 16:03:40 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import re, os, httpx
+from fake_headers import Headers
+
+
+proxfile = 'Source.txt'
+file2 = 'all.txt'
+prox = list(map(lambda x:x.strip(),open(proxfile)))
+pattern = re.compile(r"(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}:\d{2,5})")
+count = 0
+    
+def main():
+    os.remove(file2) 
+    os.system('cls' if os.name == 'nt' else 'clear')
+    headers = Headers(headers=True).generate()
+    with httpx.Client(http2=True,headers =headers) as client:
+        for proxy in range(len(prox)):
+                    r = client.get(prox[proxy],headers=headers)
+                    print(prox[proxy],r)
+                    for line in pattern.findall(r.text):
+                        f = open(file2, "a")
+                        f.write(line+'\n')
+                        f.close()
+        file = open(file2, "r")
+        mylist = list(map(lambda x:x.strip(),open(file2)))
+        mylist = list(dict.fromkeys(mylist))
+        line_count = 0
+        for line in file:
+            if line != "\n":
+                line_count += 1
+        file.close()
+        print(line_count, 'Proxies scraped, duplicates are removed')
+                    
+if __name__ == '__main__':
+    main()
