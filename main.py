4024b64f53c99b19580e394627d1a9ef24b1cddd
/⁶⁶⁶Bunti <58895443+bntr00t@users.noreply.github.com>
2022-03-06 17:25:47 +0100

    update
    added showing how many proxies gotten from each site

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ proxfile = 'Source.txt'
 file2 = 'all.txt'
 prox = list(map(lambda x:x.strip(),open(proxfile)))
 pattern = re.compile(r"(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}:\d{2,5})")
-count = 0
-    
 def main():
     if os.path.exists(file2):
         os.remove(file2)
@@ -15,20 +13,24 @@ def main():
         pass
     os.system('cls' if os.name == 'nt' else 'clear')
     headers = Headers(headers=True).generate()
-    with httpx.Client(http2=True,headers =headers) as client:
+    with httpx.Client(http2=True,headers = headers) as client:
         for proxy in range(len(prox)):
+                    line_count = 0
                     try:
                         try:                    
                             r = client.get(prox[proxy],headers=headers)
-                            print(prox[proxy],r)
+                            for line in pattern.findall(r.text):
+                                f = open(file2, "a")
+                                f.write(line+'\n')
+                                f.close()
+                                file = open(file2, "r")
+                                if line != "\n":
+                                    line_count += 1
+                            print(line_count, 'proxies gotten from source',prox[proxy],r)
                         except httpx.HTTPError as exc:
                             print(exc)
                     except:
                         pass
-                    for line in pattern.findall(r.text):
-                        f = open(file2, "a")
-                        f.write(line+'\n')
-                        f.close()
         file = open(file2, "r")
         mylist = list(map(lambda x:x.strip(),open(file2)))
         mylist = list(set(mylist))
