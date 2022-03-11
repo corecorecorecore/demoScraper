5efc9cddb566f92741ffbad0ea14167abe558342
/⁶⁶⁶Bunti <58895443+bntr00t@users.noreply.github.com>
2022-03-11 18:15:44 +0100

    fix
    switched from fake_headers to fake_useragent because of an issue of some headers not getting proxies on some sites

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 import re, os, httpx
-from fake_headers import Headers
+from fake_useragent import UserAgent
 
 
 proxfile = 'Source.txt'
 file2 = 'all.txt'
 prox = list(map(lambda x:x.strip(),open(proxfile)))
 pattern = re.compile(r"(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}:\d{2,5})")
+ua = UserAgent()
 def main():
     if os.path.exists(file2):
         os.remove(file2)
     else:
         pass
     os.system('cls' if os.name == 'nt' else 'clear')
-    headers = Headers(headers=True).generate()
+    headers = {'User-Agent':str(ua.chrome)}
     with httpx.Client(http2=True,headers = headers) as client:
         for proxy in range(len(prox)):
                     line_count = 0
                     try:
                         try:                    
-                            r = client.get(prox[proxy],headers=headers)
+                            r = client.get(prox[proxy])
                             for line in pattern.findall(str(r.content)):
-                                f = open(file2, "a")
+                                f = open(file2, "a+")
                                 f.write(line+'\n')
                                 f.close()
-                                file = open(file2, "r")
                                 if line != "\n":
                                     line_count += 1
                             print(line_count, 'proxies gotten from source',prox[proxy],r)
@@ -31,10 +31,9 @@ def main():
                             print(exc, prox[proxy])
                             r = client.get(prox[proxy],headers=headers)
                             for line in pattern.findall(r.text):
-                                f = open(file2, "a")
+                                f = open(file2, "a+")
                                 f.write(line+'\n')
                                 f.close()
-                                file = open(file2, "r")
                                 if line != "\n":
                                     line_count += 1
                             print(line_count, 'proxies gotten from source',prox[proxy],r)
